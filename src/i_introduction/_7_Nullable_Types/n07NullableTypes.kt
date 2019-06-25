a0b50ca324e07146a10be806b290316025ef6443
zhenian <zhenian@qq.com>
2019-06-25 23:58:21 +0800

    Pass 2

--- src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
+++ src/i_introduction/_7_Nullable_Types/n07NullableTypes.kt
@@ -25,7 +25,9 @@ fun todoTask7(client: Client?, message: String?, mailer: Mailer): Nothing = TODO
 fun sendMessageToClient(
     client: Client?, message: String?, mailer: Mailer
 ) {
-    todoTask7(client, message, mailer)
+    if(message==null)return
+    val email = client?.personalInfo?.email ?: return
+    mailer.sendMessage(email, message)
 }
 
 class Client(val personalInfo: PersonalInfo?)
--- src/ii_collections/n21Partition.kt
+++ src/ii_collections/n21Partition.kt
@@ -12,5 +12,8 @@ fun example8() {
 
 fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
     // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
+    return this.customers.filter {
+        val (d, un) = it.orders.partition { it.isDelivered };
+        d.count() < un.count()
+    }.toSet()
 }
