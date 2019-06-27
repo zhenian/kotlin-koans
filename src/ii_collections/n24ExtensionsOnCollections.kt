3b892487574a1dfe84c13fca0f3555c150fd1929
zhenian <zhenian@qq.com>
2019-06-27 12:01:36 +0800

    pass 5

--- src/ii_collections/n24ExtensionsOnCollections.kt
+++ src/ii_collections/n24ExtensionsOnCollections.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import com.google.common.collect.Lists
+import com.google.common.collect.Maps
 import util.TODO
 
 fun todoTask24(): Nothing = TODO(
@@ -11,9 +13,35 @@ fun todoTask24(): Nothing = TODO(
     references = { c: Collection<String> -> _24_JavaCode().doSomethingStrangeWithCollection(c) }
 )
 
-fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
-    val groupsByLength = collection.groupBy { s -> todoTask24() }
+fun doSomethingStrangeWithCollection2(collection: Collection<String>): Collection<String>? {
+    val groupsByLength = Maps.newHashMap<Int, MutableList<String>>()
+    for (s in collection) {
+        var strings: MutableList<String>? = groupsByLength[s.length]
+        if (strings == null) {
+            strings = Lists.newArrayList()
+            groupsByLength[s.length] = strings
+        }
+        strings!!.add(s)
+    }
 
-    return groupsByLength.values.maxBy { group -> todoTask24() }
+    var maximumSizeOfGroup = 0
+    for (group in groupsByLength.values) {
+        if (group.size > maximumSizeOfGroup) {
+            maximumSizeOfGroup = group.size
+        }
+    }
+
+    for (group in groupsByLength.values) {
+        if (group.size == maximumSizeOfGroup) {
+            return group
+        }
+    }
+    return null
+}
+
+fun doSomethingStrangeWithCollection(collection: Collection<String>): Collection<String>? {
+    val groupsByLength = collection.groupBy { s -> s.length }
+
+    return groupsByLength.values.maxBy { group -> group.count() }
 }
 
